e9299517628bf2a20158c786eacd1321c620416a
Vadym Chernyshov <vadym1927dk@gmail.com>
2024-11-15 11:01:43 +0200

    Ввід користувача

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -3,8 +3,12 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {          
+            Console.WriteLine("Please enter your first number"); //перше число
+            string strFirstValue = Console.ReadLine();
+
+            Console.WriteLine("Please enter your first second number"); //друге число
+            string strSecondValue = Console.ReadLine();            
         }
     }
 }
