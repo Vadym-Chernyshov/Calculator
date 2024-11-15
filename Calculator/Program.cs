838acc3dadbac73c8bac1f97010fdf2cb0ee94c0
Vadym Chernyshov <vadym1927dk@gmail.com>
2024-11-15 11:14:30 +0200

    Перевірка вводу користувача

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -8,7 +8,19 @@
             string strFirstValue = Console.ReadLine();
 
             Console.WriteLine("Please enter your first second number"); //друге число
-            string strSecondValue = Console.ReadLine();            
+            string strSecondValue = Console.ReadLine();
+
+            //перевірка вводу користувача та ініціалізація числових змінних за допомогою TryParse
+            //Знайшов за посиланням нижче
+            //https://learn.microsoft.com/ru-ru/dotnet/api/system.int32.tryparse?view=net-8.0
+            if (int.TryParse(strFirstValue, out int firstValue) && int.TryParse(strFirstValue, out int secondValue))
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("Incorrect input. Please try again");
+            }
         }
     }
 }
